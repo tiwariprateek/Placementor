30a556ee03243a5dd2833a8c883900301f599bcf
tiwariprateek <prateektiwari5399@gmail.com>
2020-06-03 18:58:23 +0530

    Added runtime permissions to ResourceFragment

--- app/src/main/java/com/example/placementor/resources/ResourcesFragment.kt
+++ app/src/main/java/com/example/placementor/resources/ResourcesFragment.kt
@@ -1,8 +1,11 @@
 package com.example.placementor.resources
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DownloadManager
 import android.content.Context
 import android.content.Context.DOWNLOAD_SERVICE
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
@@ -10,6 +13,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -29,25 +33,59 @@ class ResourcesFragment : Fragment() {
     lateinit var resourceAdapter: ResourceAdapter
     val resources=ArrayList<Resources>()
     private lateinit var sharedViewModel: StudentSharedViewModel
+    private val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE=1001
+    var permission=false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         sharedViewModel= ViewModelProvider(requireActivity()).get(StudentSharedViewModel::class.java)
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
+            )
+
+        } else {
+            permission=true
+        }
         sharedViewModel.Resources.observe(viewLifecycleOwner, Observer { resource ->
             resourceAdapter=ResourceAdapter(requireContext(),resources,ResourceAdapter.OnClickListener{
-                val url=it.downloadUrl
-                val request=DownloadManager.Request(Uri.parse(url))
-                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE )
-                request.setTitle("${it.name}")
-                request.setDescription("Downloading Previous year placement papers")
-                request.allowScanningByMediaScanner()
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,""+System.currentTimeMillis())
-                val donwloadmanager=requireActivity().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-                donwloadmanager.enqueue(request)
-
+                if (permission){
+                    val url=it.downloadUrl
+                    val request=DownloadManager.Request(Uri.parse(url))
+                    request.apply {
+                        setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE )
+                        setTitle("${it.name}")
+                        setDescription("Downloading Previous year placement papers")
+                        allowScanningByMediaScanner()
+                        setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                        setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,""+System.currentTimeMillis())
+                    }
+                    val downloadmanager=requireActivity().getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+                    downloadmanager.enqueue(request)
+                }
+                else{
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+                    builder.setMessage("Without permission we won't be abe to unlock our whole potential." +
+                            "Kindly grant the permission to have best experience")
+                        .setCancelable(false)
+                        .setPositiveButton("OK"
+                        ){ _, _ ->
+                            requestPermissions(
+                                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                                MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
+                            )
+                        }
+                    val alert: AlertDialog = builder.create()
+                    alert.show()
+                }
             })
             val decoration= ItemDecoration(30)
             jobs_recyclerview.addItemDecoration(decoration)
@@ -61,6 +99,18 @@ class ResourcesFragment : Fragment() {
         })
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_resources, container, false)
+    }
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
+                permission = (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 
 }
